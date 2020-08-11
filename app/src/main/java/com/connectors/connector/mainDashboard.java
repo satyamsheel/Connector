71707b64f7901b29c5ba33d8998ccc2dad7d3486
satyam sheel <sheelsatyam@ymail.com>
2020-08-11 22:43:02 +0530

    viewPager Added

--- app/src/main/java/com/connectors/connector/mainDashboard.java
+++ app/src/main/java/com/connectors/connector/mainDashboard.java
@@ -3,14 +3,26 @@ package com.connectors.connector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-public class    mainDashboard extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class mainDashboard extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dashboard);
 
+        toolbar=(Toolbar) findViewById(R.id.main_app_bar);
+        setSupportActionBar(toolbar);
 
     }
+
 }
