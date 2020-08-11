5811120b9206e4c36bbc3e4b9ae0b830b2073b3f
satyam sheel <sheelsatyam@ymail.com>
2020-08-11 20:51:46 +0530

    dashboard update

--- app/src/main/java/com/connectors/connector/mainDashboard.java
+++ app/src/main/java/com/connectors/connector/mainDashboard.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class mainDashboard extends AppCompatActivity {
+public class    mainDashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dashboard);
+
+
     }
 }
