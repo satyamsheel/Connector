9f5d0bcc9544db8fa1f4a3847de0911ca7633ab3
satyam sheel <sheelsatyam@ymail.com>
2020-09-03 19:57:58 +0530

    profile image crop

--- app/src/main/java/com/connectors/connector/settings.java
+++ app/src/main/java/com/connectors/connector/settings.java
@@ -1,13 +1,18 @@
 package com.connectors.connector;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Gallery;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -16,6 +21,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,6 +34,7 @@ public class settings extends AppCompatActivity {
     DocumentReference rootref;
     private FirebaseAuth mAuth;
     FirebaseFirestore db;
+    final int galPick=1;
 
 
     @Override
@@ -60,13 +68,36 @@ public class settings extends AppCompatActivity {
             }
         });
 
-
-
-
+        profile_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent=new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,galPick);
+            }
+        });
     }
-
     public void updateSettings(View view) {
         rootref.update("Full Name",settings_userName.getText().toString());
         rootref.update("Status",settings_status.getText().toString());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==galPick && resultCode==RESULT_OK && data!=null){
+            Uri imageUri=data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            }
+
+        }
     }
 }
