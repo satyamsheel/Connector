1ac00ab65754ac222b9259eff395b201b2eaff4a
satyam sheel <sheelsatyam@ymail.com>
2020-09-06 00:33:47 +0530

    Retrive contact

--- app/src/main/java/com/connectors/connector/contacts.java
+++ app/src/main/java/com/connectors/connector/contacts.java
@@ -3,15 +3,16 @@ package com.connectors.connector;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.MenuItem;
-import android.view.View;
-
+import android.widget.TextView;
 
 public class contacts extends AppCompatActivity {
     Toolbar toolbar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +22,41 @@ public class contacts extends AppCompatActivity {
         toolbar=findViewById(R.id.contacts_toolbar);
         setSupportActionBar(toolbar);
 
+        textView=findViewById(R.id.textView3);
+
+
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        StringBuilder builder =new StringBuilder();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
+
+        if(cursor.getCount()>0){
+            while(cursor.moveToNext()){
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                int number = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+
+                if(number>0){
+                    Cursor cursor1=contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID+"=?",new String[]{id},null);
+
+                    while (cursor1.moveToNext()){
+                        String phoneNumber=cursor1.getString(cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        builder.append("Contact:").append(name).append("Number:").append(phoneNumber).append("\n\n");
+                    }
+                    cursor1.close();
+                }
+            }
+            cursor.close();
+            textView.setText(builder.toString());
         }
     }
 
